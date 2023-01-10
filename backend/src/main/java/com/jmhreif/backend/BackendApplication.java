3e28a16f222412c46ecf7871cbf6ed78fc9c72ec
Jennifer Reif <jennifer.reif@neo4j.com>
2023-01-10 15:08:54 -0600

    Initial commit for part-one branch

--- backend/src/main/java/com/jmhreif/backend/BackendApplication.java
+++ backend/src/main/java/com/jmhreif/backend/BackendApplication.java
@@ -1,18 +1,11 @@
 package com.jmhreif.backend;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.neo4j.core.schema.Id;
-import org.springframework.data.neo4j.core.schema.Node;
-import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-
-import java.time.ZonedDateTime;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -24,25 +17,9 @@ public class BackendApplication {
 }
 
 @RestController
-@RequestMapping("/neo")
+@RequestMapping("/text")
 @AllArgsConstructor
 class MessageController {
-	private final ReviewRepository repo;
-
 	@GetMapping
-	Flux<Review> getReviews() { return repo.findFirst1000By(); }
-}
-
-interface ReviewRepository extends ReactiveNeo4jRepository<Review, String> {
-	Flux<Review> findFirst1000By();
-}
-
-@Data
-@Node
-class Review {
-	@Id
-	private String review_id;
-	private String book_id, review_text;
-	private ZonedDateTime date_added;
-	private Integer rating;
+	String getText() { return "Hello, World!"; }
 }
--- frontend/src/main/java/com/jmhreif/frontend/FrontendApplication.java
+++ frontend/src/main/java/com/jmhreif/frontend/FrontendApplication.java
@@ -1,8 +1,6 @@
 package com.jmhreif.frontend;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,38 +8,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
-
-import java.time.ZonedDateTime;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class FrontendApplication {
-	@Value("${backend.hostname:localhost}")
-	private String hostname;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendApplication.class, args);
 	}
 
 	@Bean
-	WebClient client() { return WebClient.create("http://" + hostname + ":8081"); }
+	WebClient client() { return WebClient.create("http://localhost:8081"); }
 
 }
 
 @RestController
-@RequestMapping("/reviews")
+@RequestMapping("/hello")
 @AllArgsConstructor
-class ReviewController {
+class TextController {
 	private final WebClient client;
 
 	@GetMapping
-	Flux<Review> getReviews() { return client.get().uri("/neo").retrieve().bodyToFlux(Review.class); }
-}
-
-@Data
-class Review {
-	private String review_id;
-	private String book_id, review_text;
-	private ZonedDateTime date_added;
-	private Integer rating;
+	Mono<String> getText() { return client.get().uri("/text").retrieve().bodyToMono(String.class); }
 }
